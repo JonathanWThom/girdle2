6bad6d9a1bcbe7e666836ba32f42c3e10317481b
Brad Copenhaver and Jonathan Thom <jonathan@>
2017-01-10 09:15:52 -0800

    add admin portal with delete function

---
+++ app/components/admin-tile.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions:{
+    edit(article, params){
+      this.sendAction('edit', article, params);
+    },
+    delete(article){
+      if (confirm('Are you sure you want to delete this article?')) {
+        this.sendAction('delete', article);
+      }
+    }    
+  }
+});
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('article');
+  },
+  actions: {
+    edit(article, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          article.set(key,params[key]);
+        }
+      });
+      article.save();
+      this.transitionTo('admin');
+    },
+    delete(article) {
+      article.destroyRecord();
+      this.transitionTo('admin');
+    }
+  }
 });
